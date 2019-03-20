0705ed52a27aa508e25f5b45c6b971605d037b3b
Edoardo Tenani <edoardo.tenani@gmail.com>
2019-03-20 17:08:45 +0100

    more aliases

--- home/.config/dotfiles/sources/50_terraform.zsh
+++ home/.config/dotfiles/sources/50_terraform.zsh
@@ -1,10 +1,18 @@
 alias tf=terraform
-alias tfp="tf plan"
+# terraform apply
 alias tfa="tf apply"
 alias tfat="tfa --target"
 alias tfaa="tfa --auto-approve"
+# terraform state
+alias tfs="tf state"
+alias tfsm="tfs mv"
+alias tfsr="tfs rm"
+# terraform workspace
 alias tfw="tf workspace"
 alias tfwn="tfw new"
 alias tfws="tfw select"
 alias tfwl="tfw list"
+
+alias tfi="tf init"
 alias tfo="tf output"
+alias tfp="tf plan"
